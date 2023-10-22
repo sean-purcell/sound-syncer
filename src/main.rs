a9119449ed9baa52eca26986f512e9d9573ada9a
Sean Purcell <me@seanp.xyz>
2023-10-21 23:07:11 -0400

    Podcast progress

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::fs;
-use std::process::{Command, Stdio};
+use std::process::Stdio;
 
 use clap::Parser;
+use eyre::{Report, Result, WrapErr};
 use serde::{Deserialize, Serialize};
+use tokio::fs;
+use tokio::process::Command;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Playlist {
@@ -12,8 +14,10 @@ struct Playlist {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Podcast {
+    name: String,
     url: String,
     keep_latest: u32,
+    playback_speed: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -37,9 +41,11 @@ struct Args {
     output_dir: String,
 }
 
-fn sync_playlist(playlist: &Playlist, output_dir: &str) {
+async fn sync_playlist(playlist: &Playlist, output_dir: &str) -> Result<()> {
     let playlist_dir = format!("{output_dir}/{0}", playlist.name);
-    fs::create_dir_all(&playlist_dir).expect("Failed to create directory for playlist");
+    fs::create_dir_all(&playlist_dir)
+        .wrap_err("Failed to create directory for playlist")
+        .await?;
     let status = Command::new("spotdl")
         .current_dir(&playlist_dir)
         .stdin(Stdio::null())
@@ -48,7 +54,8 @@ fn sync_playlist(playlist: &Playlist, output_dir: &str) {
         .arg("--save-file")
         .arg("playlist.spotdl")
         .status()
-        .expect("Failed to execute spotdl");
+        .await
+        .wrap_err("Failed to execute spotdl");
     if status.success() {
         println!("Synced playlist");
     } else {
@@ -56,13 +63,14 @@ fn sync_playlist(playlist: &Playlist, output_dir: &str) {
     }
 }
 
-fn sync_podcasts(podcasts: &PodcastSet, output_dir: &str) {
+async fn sync_podcasts(podcasts: &PodcastSet, output_dir: &str) {
     let playlist_dir = format!("{output_dir}/{0}", podcasts.name);
     fs::create_dir_all(&playlist_dir).expect("Failed to create directory for podcast set");
     for podcast in &podcasts.podcasts {}
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
     println!("Loading config: {0}", args.config);
 
